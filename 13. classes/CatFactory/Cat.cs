1fb90076c8ac3b98f504be9ade913451db8ec8da
Alex <iseldomused@gmail.com>
2018-05-10 14:51:50 +0300

    upd 13

--- 13. classes/CatFactory/Cat.cs
+++ 13. classes/CatFactory/Cat.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CatFactory {
+﻿namespace CatFactory {
 	class Cat {
 		public string name;
 		public double age;
 		public double weight;
 		public double strenght;
-
-		Cat(string name, double age, double weight, double strenght) {
-
-		}
 	}
 }
