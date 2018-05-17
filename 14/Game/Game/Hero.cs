46717784e4ad5e44b751384438ebe080f91c28f7
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-05-17 20:18:37 +0300

    14

---
+++ 14/Game/Game/Hero.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Hero
+    {
+        public bool isAttacking;
+        public int hp;
+        public int dmg;
+        public string name;
+        public bool isEnemy;
+        public int maxHp;
+        private bool isSelected;
+        public ConsoleColor color = ConsoleColor.White;
+
+        public Hero(string grade,bool isEnemy,int dmg,int hp)
+        {
+            this.name = grade;
+            this.isEnemy = isEnemy;
+            this.dmg = dmg;
+            this.hp = hp;
+            maxHp = hp;
+            
+        }
+        public void IsAttacking(bool value)
+        {
+            this.isAttacking = value;
+            if (isAttacking)
+            {
+                color = ConsoleColor.Green;
+            }
+            else
+            {
+                color = ConsoleColor.White;
+            }
+        }
+        public void SetSelect(bool isSelected)
+        {
+            this.isSelected = isSelected;
+            if (isSelected)
+            {
+                color = ConsoleColor.Red;
+            }
+            else
+            {
+                color = ConsoleColor.White;
+            }
+        }
+        public void TakeDamage(int enemyDmg){
+            hp = hp - enemyDmg;
+        }
+        public void Draw(int position)
+        {
+            if (isEnemy)
+            {
+                Console.SetCursorPosition(40, position);
+            }
+            else
+            {
+                Console.SetCursorPosition(5, position);
+            }
+            Console.ForegroundColor = color;
+            Console.Write(name + " " + hp.ToString() + "/" + maxHp.ToString());
+ 
+        }
+    }
+}
---
+++ 14/Game/Game/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int sideTurn = 1;
+        static int whoseTurn = 0;
+        static int selectedEnemy = 0;
+        static List<Hero> heroes = new List<Hero>();
+        static List<Hero> monsters = new List<Hero>();
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                Hero heroe = new Hero("воин", false, 5, 20);
+                heroes.Add(heroe);
+                Hero monster = new Hero("монстр", true, 5, 20);
+                monsters.Add(monster);
+
+            }
+            heroes[0].IsAttacking(true);
+            monsters[selectedEnemy].SetSelect(true);
+            while (true)
+            {
+
+                while (!Console.KeyAvailable)
+                {
+                    Console.Clear();
+                    Update();
+                    Thread.Sleep(50);
+                }
+                if (sideTurn == 1)
+                {
+                    Turn(heroes, monsters);
+                }
+                else
+                {
+                    Turn(monsters, heroes);
+                }
+            }
+        }
+
+        private static void Turn(List<Hero> attackers, List<Hero> defenders)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            EnemySelect(keyInfo, defenders);
+            if (keyInfo.Key == ConsoleKey.Spacebar)
+            {
+                attackers[whoseTurn].IsAttacking(false);
+                defenders[selectedEnemy].TakeDamage(attackers[whoseTurn].dmg);
+                whoseTurn = whoseTurn + 1;
+            }
+            if (whoseTurn == attackers.Count)
+            {
+                defenders[selectedEnemy].SetSelect(false);
+                whoseTurn = 0;
+                sideTurn = sideTurn * -1;
+                defenders[selectedEnemy].SetSelect(false);
+                
+                selectedEnemy = 0;
+                attackers[0].SetSelect(true);
+                defenders[0].IsAttacking(true);
+            }
+            else
+            {
+                attackers[whoseTurn].IsAttacking(true);
+            }
+        }
+
+        static void Update()
+        {
+
+            DrawSides(heroes);
+            DrawSides(monsters);
+            if (sideTurn==1)
+            {
+                heroes[whoseTurn].IsAttacking(true);
+            }
+            else
+            {
+                monsters[whoseTurn].IsAttacking(true);
+            }
+        }
+
+        private static void DrawSides(List<Hero> heroes)
+        {
+            for (int i = 0; i < heroes.Count; i++)
+            {
+                if (heroes[i].hp < 1)
+                {
+                    heroes.RemoveAt(i);
+                    selectedEnemy = 0;
+                    heroes[selectedEnemy].SetSelect(true);
+                    i = i - 1;
+                }
+                else
+                {      
+                    heroes[i].Draw(i);
+                }
+
+            }
+        }
+        static void EnemySelect(ConsoleKeyInfo keyInfo, List<Hero> enemy)
+        {
+            enemy[selectedEnemy].SetSelect(false);
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+
+                if (selectedEnemy == 0)
+                {
+                    selectedEnemy = enemy.Count - 1;
+                }
+                else
+                {
+                    selectedEnemy = selectedEnemy - 1;
+                }
+
+            }
+            if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+
+                if (selectedEnemy == enemy.Count - 1)
+                {
+                    selectedEnemy = 0;
+                }
+                else
+                {
+                    selectedEnemy = selectedEnemy + 1;
+                }
+
+            }
+            enemy[selectedEnemy].SetSelect(true);
+        }
+    }
+}
